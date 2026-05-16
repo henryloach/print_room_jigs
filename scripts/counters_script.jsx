5d4f2c532067d4e4aaf9c2636ca0eab78acbfa19
Chris Loach <chenryloach@gmail.com>
2026-05-16 13:57:02 +0100

    eddit text data

--- scripts/counters_script.jsx
+++ scripts/counters_script.jsx
@@ -2,33 +2,33 @@ const fontData = {
     "Script": {
         "fullName": "MonotypeCorsiva", // work
         // "fullName": "Monotype-Corsiva-Regular", // home
-        "sizeFactor": 1,
-        "y-nudge": 0.25 / 8.0,
+        "sizeFactor": 0.80,
+        "y-nudge": -1.2 / 8.0,
     },
     "Classic Script": {
         "fullName": "DancingScript-Regular",
-        "sizeFactor": 1.0625,
-        "y-nudge": 0.2 / 8.0,
+        "sizeFactor": 0.75,
+        "y-nudge": -0.2 / 8.0,
     },
     "Italic": {
         "fullName": "GentiumBookPlus-Italic",
-        "sizeFactor": 1,
-        "y-nudge": 0.9 / 8.0,
+        "sizeFactor": 0.85,
+        "y-nudge": -1.2 / 8.0,
     },
     "Serif": {
         "fullName": "Merriweather-Regular",
         "sizeFactor": 0.7,
-        "y-nudge": - 1.0 / 8.0,
+        "y-nudge": -0.8 / 8.0,
     },
     "Sans Serif": {
         "fullName": "Roboto-Medium",
         "sizeFactor": 0.65,
-        "y-nudge": - 1.0 / 8.0,
+        "y-nudge": -1.0 / 8.0,
     },
     "Handwritten": {
         "fullName": "Caveat-Regular",
-        "sizeFactor": 1.125,
-        "y-nudge": 0.3 / 8.0,
+        "sizeFactor": 0.8,
+        "y-nudge": -0.4 / 8.0,
     }
 }
 
@@ -294,7 +294,6 @@ function generateDocument() {
 
             // Arcs
             var arcRadius = mmToPoints(activeJig.arcRadius)
-
 
             var arcGap = 8
             var leftArcStart = 90 + ( textAngleDegrees / 2 ) + arcGap - (arcFactor * textWidth - 1.1)
